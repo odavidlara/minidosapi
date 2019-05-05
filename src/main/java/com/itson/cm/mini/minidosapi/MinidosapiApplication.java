6b8c783df937f93249818f82cd8dbdedce9a4fd2
David Lara <oscar.lr97@gmail.com>
2019-05-05 17:43:46 -0500

    First Commit

---
+++ src/main/java/com/itson/cm/mini/minidosapi/MinidosapiApplication.java
@@ -0,0 +1,13 @@
+package com.itson.cm.mini.minidosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itson/cm/mini/minidosapi/MinidosapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itson.cm.mini.minidosapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinidosapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
