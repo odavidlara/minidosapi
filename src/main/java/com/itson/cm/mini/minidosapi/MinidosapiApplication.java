f53899efe18975f4548354b61aeae51ec73d5179
David Lara <oscar.lr97@gmail.com>
2019-05-12 18:46:10 -0500

    Update MinidosapiApplication.java
    cors enabled

--- src/main/java/com/itson/cm/mini/minidosapi/MinidosapiApplication.java
+++ src/main/java/com/itson/cm/mini/minidosapi/MinidosapiApplication.java
@@ -2,12 +2,28 @@ package com.itson.cm.mini.minidosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MinidosapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinidosapiApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/checkinUser").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/api/removehost").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/api/currenthost").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
