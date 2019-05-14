7cae78b021ada7918ec39c56b2e12a18b96aa82a
David Lara <oscar.lr97@gmail.com>
2019-05-13 20:32:33 -0500

    Update ApiController.java
    if there's already a host, don't allow a new one

--- src/main/java/com/itson/cm/mini/minidosapi/ApiController.java
+++ src/main/java/com/itson/cm/mini/minidosapi/ApiController.java
@@ -16,60 +16,62 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itson.cm.mini.minidosapi.models.Host;
+
 @CrossOrigin
 @RestController
 public class ApiController {
-	
+
 	private static final String template = "Hello, %s!";
-	
+
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	private List<Host> listOfHost = new ArrayList<Host>();
-	
+
 	private Host currentHost = null;
-	
+
 	public double distanceInKms(double lat1, double lon1, double lat2, double lon2) {
-		if((lat1 == lat2) && (lon1 == lon2)) {
+		if ((lat1 == lat2) && (lon1 == lon2)) {
 			return 0.0;
-		}else {
+		} else {
 			double theta = lon1 - lon2;
-			double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+			double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+					+ Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
 			dist = Math.acos(dist);
 			dist = Math.toDegrees(dist);
 			dist = dist * 60 * 1.1515;
 			dist = dist * 1.609344;
-			
+
 			return dist;
 		}
 	}
-	
-	
+
 	@CrossOrigin
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return new Greeting(counter.incrementAndGet(),
-				String.format(template, name));
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
 	@CrossOrigin
-	@RequestMapping(value="/testin", method = RequestMethod.GET)
+	@RequestMapping(value = "/testin", method = RequestMethod.GET)
 	public HashMap<String, Object> testin() {
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("key1", "some text");
 		map.put("foo", "bar");
 		return map;
 	}
-	
+
 	@CrossOrigin
 	@GetMapping("/api/currenthost")
 	@ResponseBody
-	public HashMap<String, Object> currentHost(){
+	public HashMap<String, Object> currentHost() {
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("responseCode", "404");
 		map.put("responseText", "error ");
-		if(currentHost == null) {
+
+		if (currentHost == null) {
 			map.put("responseCode", "404");
 			map.put("responseText", "host not found");
-		}else {
+		} else {
 			map.put("responseCode", "200");
 			map.put("responseText", "host found");
 			map.put("hostName: ", currentHost.getSurname());
@@ -79,62 +81,63 @@ public class ApiController {
 		return map;
 
 	}
-	
+
 	@DeleteMapping("/api/removehost")
 	@ResponseBody
-	public HashMap<String, Object> removeHost(){
+	public HashMap<String, Object> removeHost() {
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("responseCode", "200");
 		map.put("responseText", "host has been removed");
 		currentHost = null;
 		return map;
 	}
-	
+
 	@PostMapping("/api/checkinUser")
 	@ResponseBody
 	public HashMap<String, Object> checkIn(@RequestParam(name = "isHost", defaultValue = "false") boolean isHost,
-			@RequestParam(name="surname", defaultValue = "generic") String surname,
-			@RequestParam(required = true)double lat,
-			@RequestParam(required= true) double lon) {
-		
+			@RequestParam(name = "surname", defaultValue = "generic") String surname,
+			@RequestParam(required = true) double lat, @RequestParam(required = true) double lon) {
+
 		String responseCode = "404";
 		String responseText = "there was an error";
-		
-		if(isHost) {
+
+		if (isHost) {
 			Host host = new Host(surname, lat, lon);
-//			if(host.getSurname() == currentHost.getSurname()) {
-//				responseCode = "300";
-//				responseText = "host already located";
-//			}else {
+			if (currentHost == null) {
 				this.currentHost = host;
-				responseCode = "200";
-				responseText = "a host has been registered";
-			//}
-			
-			//listOfHost.add(host);
-		}else {
-			if(currentHost == null) {
+			} else {
+				if (host.getSurname() == currentHost.getSurname()) {
+					responseCode = "300";
+					responseText = "host already located";
+				} else {
+					this.currentHost = host;
+					responseCode = "200";
+					responseText = "a host has been registered";
+				}
+			}
+
+		} else {
+			if (currentHost == null) {
 				responseCode = "300";
 				responseText = "There's no host available";
-			}else {
+			} else {
 				double dist = distanceInKms(lat, lon, currentHost.getLan(), currentHost.getLon());
-				if (dist < 50) { 
+				if (dist < 50) {
 					responseCode = "200";
 					responseText = "good to go";
-				}else {
+				} else {
 					responseCode = "305";
 					responseText = "too far from host";
 				}
 			}
 		}
-		
+
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("responseCode", responseCode);
 		map.put("responseText", responseText);
-		
+
 		return map;
-		
-		}
-	
-	
+
+	}
+
 }
